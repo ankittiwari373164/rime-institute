c1e7a773dc469404d9b5bfff28203d292dce5088
ankittiwari373164 <ankittiwari.373164@gmail.com>
2026-04-01 17:40:25 +0530

    Update QuickAction.jsx

--- src/components/Admission/QuickAction.jsx
+++ src/components/Admission/QuickAction.jsx
@@ -13,7 +13,7 @@ const QuickAction = () => {
               <ArrowRight className="w-5 h-5" />
             </button>
             </Link>
-            <a href="/brochure.pdf" download>
+            <a href="/Rime Brochure (2).pdf" download>
             <button className="flex items-center gap-2 cursor-pointer bg-gray-100 text-gray-700 px-6 py-3 rounded-lg font-semibold hover:bg-gray-200 transition-colors">
               <Download className="w-5 h-5" />
               Download Prospectus
