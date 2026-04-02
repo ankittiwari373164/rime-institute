0fc95490c94d612e482b96adc3d54e2921655c76
ankittiwari373164 <ankittiwari.373164@gmail.com>
2026-04-02 17:52:34 +0530

    Update Admission.jsx

--- src/pages/Admission.jsx
+++ src/pages/Admission.jsx
@@ -7,6 +7,7 @@ import FeeStructure from "../components/Admission/FeeStructure";
 import ScholarshipFinancialAid from "../components/Admission/ScholarshipFinancialAid";
 import AdmissionContact from "../components/Admission/AdmissionContact";
 import { Helmet } from "react-helmet-async";
+import { useSEO } from '../hooks/useSEO';
 const Admission = () => {
   return (
     <>
@@ -18,6 +19,7 @@ const Admission = () => {
         />
       </Helmet>
       <div className="mt-30">
+        useSEO();
         <HeroAdmission />
         <QuickAction />
         <AdmissionProcess />
