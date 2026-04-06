387eaf021719786b4f76045f51e80118cd74d16b
ankittiwari373164 <ankittiwari.373164@gmail.com>
2026-04-06 21:02:16 +0530

    Update CardInfo.jsx

--- src/components/Contact/CardInfo.jsx
+++ src/components/Contact/CardInfo.jsx
@@ -11,7 +11,7 @@ const CardInfo = () => {
     {
       icon: Phone,
       title: "Phone",
-      details: ["1800-833-3338 (Toll Free)", "+91-8810676939", "+91-8810676939"],
+      details: [ "+91-8810676939", "+91-8810676939"],
     },
     {
       icon: Mail,
